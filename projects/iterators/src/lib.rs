5b4aef867b996efd81f6c4ce284e4bc9e003115d
Usman Akinyemi <usmanakinyemi202@gmail.com>
2025-07-28 21:21:37 +0530

    studied closure, iterators and minigrep in rust

---
+++ projects/iterators/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn flatten<O>(iter: O) -> Flatten<I> {
+    Flatten::new(iter)
+}
+
+pub struct Flatten<O> {
+    outer: O,
+}
+
+impl<O> Flatten<O> {
+    fn new(iter: O) -> Self {
+        Flatten { outer: iter }
+    }
+}
+
+impl<O> Iterator for Flatten<O>
+where
+    O: Iterator,
+    O::Item: IntoIterator,
+{
+    type Item = <O::Item as IntoIterator>::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.outer.next().and_then(|inner| inner.into_iter().next())
+    }
+}
--- projects/minigrep/src/lib.rs
+++ projects/minigrep/src/lib.rs
@@ -1,27 +1,99 @@
 use ::std::error::Error;
+use std::env;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With Text\n {contents}");
+    let result = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    for line in result {
+        println!("{line}");
+    }
+    // println!("With Text\n {contents}");
     Ok(())
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next()
         if args.len() < 3 {
             return Err("not enough parameters");
         }
 
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        println!("{ignore_case}");
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(query.to_lowercase().as_str()) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
